9c4f7e5ec6503f6a12c4d2a9eedf64d31b4bb528
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-09 23:55:58 +0530

    Created variables for jump force and gravity modifier

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
     // Start is called before the first frame update
     void Start()
     {
