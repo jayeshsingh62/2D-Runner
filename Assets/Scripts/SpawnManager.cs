eacbfcd8a58625e46bc9113534936fa5cbac2df0
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-10 14:38:06 +0530

    Configured the script

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(obstaclePrefab, spawnPos, transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+          
     }
 }
