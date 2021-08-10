5c7197a9aebf80378476061f03d0ed1286cd7f8c
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-10 15:38:58 +0530

    Added a reference to player controller script using a variable

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 30;
+    private PlayerController playerControllerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();    
     }
 
     // Update is called once per frame
