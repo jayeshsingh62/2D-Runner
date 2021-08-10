d2f06e116b72191c0d6a4016bd62c14e2acaa3fe
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-10 15:40:56 +0530

    Move left as long as the gameOver is false

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -16,6 +16,9 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
     }
 }
