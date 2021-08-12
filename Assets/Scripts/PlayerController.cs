a998de77a1d67a0e2b8c25884edb0e7d57f4a7b8
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-12 22:45:55 +0530

    Added the jump and crash sounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
     private AudioSource playerAudio;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
 
     // Start is called before the first frame update
     void Start()
