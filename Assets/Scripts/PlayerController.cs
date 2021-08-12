1fda5518c8fa3e10a1c7f83c17157fbd8f1ae200
Jayesh Singh <jayeshsingh62@gmail.com>
2021-08-12 21:12:50 +0530

    Make the player transition into death animation on dying

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -42,6 +42,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
